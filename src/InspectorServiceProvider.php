841d4321a3405d0a8db86dc8449919b79387ca7d
Sabar Tampubolon <stampubolon@dyned.com>
2018-12-06 09:28:07 +0700

    updated service for lumen

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -7,11 +7,52 @@ use Illuminate\Support\ServiceProvider;
 class InspectorServiceProvider extends ServiceProvider
 {
     /**
+     * The Laravel application instance.
+     *
+     * @var \Illuminate\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * Normalized Laravel Version
+     *
+     * @var string
+     */
+    protected $version;
+
+    /**
      * True when booted.
      *
      * @var bool
      */
     protected $booted = false;
+
+    /**
+     * True when enabled, false disabled an null for still unknown
+     *
+     * @var bool
+     */
+    protected $enabled = null;
+
+    /**
+     * True when this is a Lumen application
+     *
+     * @var bool
+     */
+    protected $is_lumen = false;
+
+    /**
+     * @param Application $app
+     */
+    public function __construct($app = null)
+    {
+        if (!$app) {
+            $app = app();   //Fallback when $app is not given
+        }
+        $this->app      = $app;
+        $this->version  = $app->version();
+        $this->is_lumen = str_contains($this->version, 'Lumen');
+    }
 
     /**
      * Bootstrap services.
@@ -51,8 +92,10 @@ class InspectorServiceProvider extends ServiceProvider
         }
 
         // Enable inspector for request service provider using inspector config file
-        if (config('inspector.enableRequestInspector') === true) {
-            $this->app->register('MargaTampu\LaravelInspector\InspectorRequestServiceProvider');
+        if (!$this->is_lumen) { // Not allowed for lumen
+            if (config('inspector.enableRequestInspector') === true) {
+                $this->app->register('MargaTampu\LaravelInspector\InspectorRequestServiceProvider');
+            }
         }
 
         $this->commands('MargaTampu\LaravelInspector\Console\Commands\InspectorAuthorizationCommand');
